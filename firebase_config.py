eeca34f302117c4fb23408d9703de6a121ce6fa4
abhinavcheepa <abhinavcheepa7@gmail.com>
2026-05-27 17:01:55 +0530

    fix vapi args parsing + add end call

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,21 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
 
-cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS"))
-firebase_admin.initialize_app(cred)
+# File path ya JSON string dono handle karo
+firebase_creds = os.getenv("FIREBASE_CREDENTIALS")
 
+try:
+    # Pehle JSON string try karo
+    cred_dict = json.loads(firebase_creds)
+    cred = credentials.Certificate(cred_dict)
+except:
+    # File path se load karo
+    cred = credentials.Certificate(firebase_creds)
+
+firebase_admin.initialize_app(cred)
 db = firestore.client()
